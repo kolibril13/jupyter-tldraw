856f0234c9a82a4505656012f07cc83de7cbee72
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-05-03 22:30:02 +0200

    add package structure

---
+++ draw/__init__.py
@@ -0,0 +1,8 @@
+from .tldraw import TldrawWidget
+
+
+# import importlib.metadata
+
+# __version__: str = importlib.metadata.version(__name__)
+
+# print(f"Jupyter Tldraw v{__version__}")
---
+++ draw/tldraw_component.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { TDAssetType, TDShapeType, Tldraw, TldrawApp } from "@tldraw/tldraw";
+
+export default function App({ image_width, image_height, base64img }) {
+  const handleMount = (app: TldrawApp) => {
+    app.patchAssets({
+      myAssetId: {
+        id: "myAssetId",
+        type: TDAssetType.Image,
+        size: [image_width, image_height],
+        fileName: "card-repo.png",
+        src: base64img,
+      },
+    });
+
+    app.createShapes({
+      id: "myImage",
+      type: TDShapeType.Image,
+      assetId: "myAssetId",
+      point: [0, 60],
+      size: [image_width, image_height],
+      isLocked: true,
+    });
+  };
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: image_width,
+        height: image_height + 120,
+      }}
+    >
+      <Tldraw onMount={handleMount}  />
+    </div>
+  );
+}
