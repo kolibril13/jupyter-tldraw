f6e1d7ffadfd2a486f2a494399d1b5d2b07545bb
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-05-20 17:51:21 +0100

    small tweaks

--- tldraw/tldraw.py
+++ tldraw/tldraw.py
@@ -2,8 +2,8 @@ import ipyreact
 from traitlets import Int
 from pathlib import Path
 class TldrawWidget(ipyreact.ReactWidget):
-    width = Int(300).tag(sync=True)
-    height = Int(100).tag(sync=True)
+    width = Int(600).tag(sync=True)
+    height = Int(300).tag(sync=True)
     _esm = Path(__file__).resolve().parent  / "comp.tsx"
     
 
