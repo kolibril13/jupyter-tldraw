9f58a139e394c2e71e3fedb168ef97d9793b9f76
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-06-02 13:17:34 +0200

    ideas from call with steve

--- future_ideas/bidirectional_communation/tldraw_matplotlib_component.tsx
+++ future_ideas/bidirectional_communation/tldraw_matplotlib_component.tsx
@@ -1,25 +1,34 @@
 import { TDShapeType, Tldraw } from "@tldraw/tldraw";
 import * as React from "react";
 
-export default function App({ my_text }) {
-const handleMount = React.useCallback((app: Tldraw) => {
-    app.createShapes({
-    id: "text1",
-    type: TDShapeType.Text,
-    point: [100, 100],
-    text: my_text,
-    });
-});
+export default function App({ my_text, my_text_out }) {
 
-return (
-    <div
-    style={{
-        position: "relative",
-        width: "500px",
-        height: "350px",
-    }}
-    >
-    <Tldraw onMount={handleMount} />
-    </div>
-);
+    const [app, setApp] = React.useState()
+
+     const handleMount = React.useCallback((app: Tldraw) => {
+         setApp(app)
+     }, []);
+
+     React.useEffect(() => {
+         if (app) {
+         app.createShapes({
+             id: "text1",
+             type: TDShapeType.Text,
+             point: [100, 100],
+             text: my_text,
+         });
+         }
+     }, [my_text, app])
+
+    return (
+        <div
+        style={{
+            position: "relative",
+            width: "800px",
+            height: "350px",
+        }}
+        >
+        <Tldraw onMount={handleMount} onChange={e => console.log("hii")} />
+        </div>
+    );
 }
