632a7b9bfd4ec7634e8f21c8ef72bf4216f19ed6
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-11-13 15:30:15 +0100

    keep track of position

--- js/imagearray.jsx
+++ js/imagearray.jsx
@@ -1,30 +1,33 @@
 import * as React from "react";
-
 import { createRender, useModelState } from "@anywidget/react";
 import { AssetRecordType, Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
 
 export const render = createRender(() => {
   const [app, setApp] = React.useState(null);
-  const [image_width] = useModelState("image_width");
-  const [image_height] = useModelState("image_height");
+  const [imageWidth] = useModelState("image_width");
+  const [imageHeight] = useModelState("image_height");
   const [base64img] = useModelState("base64img");
+  const [lastImageX, setLastImageX] = React.useState(0); // State to track the X position of the last image
 
   const handleMount = React.useCallback((app) => {
     setApp(app);
   }, []);
 
   React.useEffect(() => {
-    if (app) {
+    if (app && base64img) {
       const assetId = AssetRecordType.createId();
       const placeholderAsset = {
         id: assetId,
         typeName: "asset",
         type: "image",
+
+    
         props: {
-          w: image_width,
-          h: image_height,
+          w: imageWidth,
+          h: imageHeight,
           name: "card-repo.png",
+
           isAnimated: false,
           mimeType: null,
           src: base64img,
@@ -34,18 +37,24 @@ export const render = createRender(() => {
 
       app.createAssets([placeholderAsset]);
 
+      const newX = lastImageX + 0.3*imageWidth;
+
       app.createShapes([
         {
           type: "image",
+          x: newX, 
+          y: 0, 
           props: {
-            w: image_width,
-            h: image_height,
+            w: imageWidth,
+            h: imageHeight,
             assetId: assetId,
           },
         },
       ]);
+
+      setLastImageX(newX); // Update the last image X position
     }
-  }, [base64img, app]); // This will trigger the effect when `app` changes.
+  }, [base64img, app]);
 
   return (
     <div
