73e520ddc947c60971f0c876c35e0a5b6d96fcbd
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-12-16 16:39:02 +0100

    add prompt

--- src/tldraw/__init__.py
+++ src/tldraw/__init__.py
@@ -3,8 +3,9 @@ import pathlib
 from pathlib import Path
 import base64
 import anywidget
-from traitlets import Unicode, Int
+from traitlets import Unicode, Int, observe
 import io
+from .prompt import sent_request_to_openai
 
 try:
     __version__ = importlib.metadata.version("tldraw")
@@ -95,3 +96,40 @@ class TldrawMakeStaticTldraw(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "makestatic_tldraw.js"
     _css = pathlib.Path(__file__).parent / "static" / "makestatic_tldraw.css"
     value = Int(0).tag(sync=True)
+
+
+class MakeReal(anywidget.AnyWidget):
+    api_key = Unicode("KEY").tag(sync=True)
+    width = Int(600).tag(sync=True)
+    height = Int(300).tag(sync=True)
+
+    _esm = pathlib.Path(__file__).parent / "static" / "makereal.js"
+    _css = pathlib.Path(__file__).parent / "static" / "makereal.css"
+    snapshot = Unicode("").tag(sync=True)
+
+    @observe("snapshot")
+    def _observe_count(self, change):
+        base64_image = change["new"]
+        base64_image = base64_image.replace("data:image/png;base64,", "")
+        prompt = """
+
+        You are an expert data scientist who specializes in creating scientific visualization protopyes with matplotlib from low-fidelity wireframes. Your job is to accept low-fidelity designs and turn them into stunning visualization. When sent new designs, you should reply with your best attempt at a high fidelity working prototype as a single python file.
+
+        If you have to make calculations, you can use numpy.
+
+        The designs may include flow charts, diagrams, labels, arrows, sticky notes, screenshots of other applications, or even previous designs. Treat all of these as references for your prototype. Use your best judgement to determine what is an annotation and what should be included in the final result. Treat anything in the color red as an annotation rather than part of the design. Do NOT include any red elements or any other annotations in your final result.
+
+        Your prototype should look and feel much more complete and advanced than the wireframes provided. Flesh it out, make it real! Try your best to figure out what the data scientist wants and make it happen. If there are any questions or underspecified features, use what you know about applications, user experience, and scientific visualizations to "fill in the blanks". If you're unsure of how the designs should work, take a guess—it's better for you to get it wrong than to leave things incomplete. 
+
+        Remember: you love your data scientist and want them to be happy. The more complete and impressive your prototype, the happier they will be. Good luck, you've got this!
+
+        Reply ONLY with python code.
+        """
+        result = sent_request_to_openai(prompt, base64_image, self.api_key)
+        print(result)
+
+        from ipylab import JupyterFrontEnd
+
+        app = JupyterFrontEnd()
+        app.commands.execute("notebook:insert-cell-below")
+        app.commands.execute("notebook:replace-selection", {"text": result})
---
+++ src/tldraw/prompt.py
@@ -0,0 +1,50 @@
+import requests
+
+def sent_request_to_openai(prompt, base64_image, api_key):
+
+  headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+  }
+
+  payload = {
+    "model": "gpt-4-vision-preview",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": prompt
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": f"data:image/jpeg;base64,{base64_image}"
+            }
+          }
+        ]
+      }
+    ],
+    "max_tokens": 300
+  }
+
+  print("Sending request to OpenAI API... This will take about 5 seconds.")
+  response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+  r = response.json()
+  print(r)
+  text_response = r['choices'][0]["message"]["content"]
+
+  import re
+  # Regular expression to find the block of text
+  pattern = r"```python(.*?)```"
+  match = re.search(pattern, text_response, re.DOTALL)
+
+  if match:
+      code_inside = match.group(1).strip()  # .strip() to remove leading/trailing whitespace
+      # print(code_inside)
+  else:
+      print("No code block found")
+  # print(code_inside)
+  return code_inside
