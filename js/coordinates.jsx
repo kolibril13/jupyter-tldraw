ab31ec86ab01eb6a866f99ac64cab9bfb398ca3b
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-30 14:49:43 +0100

    simple coordinates exampel

--- js/coordinates.jsx
+++ js/coordinates.jsx
@@ -1,23 +1,62 @@
 import * as React from "react";
-import { createRender, useModelState } from "@anywidget/react";
 
+import { createRender } from "@anywidget/react";
 import { Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
-import "./widget.css";
+
 export const render = createRender(() => {
-  const [width] = useModelState("width");
-  const [height] = useModelState("height");
-  const [points] = useModelState("points");
+  const [app, setApp] = React.useState(null);
+
+  React.useEffect(() => {
+    if (app) {
+      app.createShapes([
+        {
+          type: "draw",
+          rotation: 0,
+          typeName: "shape",
+          props: {
+            color: "red",
+            dash: "draw",
+            fill: "none",
+            isClosed: false,
+            segments: [
+              {
+                type: "straight",
+                points: [
+                  { x: 0, y: 0, z: 0.5 },
+                  { x: 100, y: -100, z: 0.5 },
+                ],
+              },
+              {
+                type: "straight",
+                points: [
+                  { x: 100, y: -100, z: 0.5 },
+                  { x: 300, y: 100, z: 0.5 },
+                ],
+              },
+              {
+                type: "straight",
+                points: [
+                  { x: 300, y: 100, z: 0.5 },
+                  { x: 400, y: 0, z: 0.5 },
+                ],
+              },
+            ],
+          },
+        },
+      ]);
+    }
+  }, [app]); // This will trigger the effect when `app` changes.
 
   return (
     <div
       style={{
         position: "relative",
-        width: width,
-        height: height,
+        width: "500px",
+        height: "500px",
       }}
     >
-      <Tldraw />
+      <Tldraw onMount={setApp} />
     </div>
   );
 });
