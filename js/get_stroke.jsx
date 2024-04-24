bfe006ab11dc34d5a1aa1d92d9dc8a3963634849
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-04-24 15:46:55 +0200

    better get stroke

--- js/get_stroke.jsx
+++ js/get_stroke.jsx
@@ -1,27 +1,51 @@
 import * as React from "react";
+import { useState } from "react";
 import { createRender, useModelState } from "@anywidget/react";
 
 import { Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
 import "./widget.css";
 const render = createRender(() => {
-  const [width] = useModelState("width");
-  const [height] = useModelState("height");
-
-  const handleChange = (e) => {
-    console.log("hi");
-  };
+  const [length, setLength] = useState(0);
 
   return (
-    <div
-      style={{
-        position: "relative",
-        width: width,
-        height: height,
-      }}
-    >
-      <Tldraw onChange={handleChange} />
-    </div>
+    <>
+      <div
+        style={{
+
+          position: "relative",
+          width: "500px",
+          height: "500px",
+        }}
+      >
+        <Tldraw
+          onMount={(editor) => {
+            editor.store.listen(() => {
+              let ob = editor.getCurrentPageShapesSorted();
+              if (ob.length === 0) return;
+              let lastElement = ob[ob.length - 1];
+              if (
+                lastElement.props.segments &&
+                lastElement.props.segments[0].points
+              ) {
+                // Set the length state to the length of the points array
+                setLength(lastElement.props.segments[0].points.length);
+                console.log(lastElement.props.segments[0].points.length);
+              }
+            });
+          }}
+        />
+      </div>
+      <div
+        style={{
+          position: "relative",
+          top: "-10px",
+          fontSize: "40px",
+        }}
+      >
+        Length of currently drawn stroke: {length}
+      </div>
+    </>
   );
 });
 export default { render };
