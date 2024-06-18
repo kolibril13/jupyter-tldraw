a9815affe396bab7cee1e9a86ee35ed6e2967add
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-06-18 14:30:54 +0200

    add config

---
+++ node_config.mjs
@@ -0,0 +1,27 @@
+import esbuild from "esbuild";
+
+esbuild.build({
+  entryPoints: [
+    "js/bidirectional_stroke.jsx",
+    "js/coordinates.jsx",
+    "js/get_stroke.jsx",
+    "js/image_and_array.jsx",
+    "js/image.jsx",
+    "js/makereal.jsx",
+    "js/makestatic_png.jsx",
+    "js/makestatic_svg.jsx",
+    "js/makestatic_tldraw.jsx",
+    "js/makestatic_to_markdown.jsx",
+    "js/minimal.jsx",
+  ],
+  bundle: true,
+  minify: true,
+  target: ["es2020"],
+  outdir: "src/tldraw/static/",
+  format: "esm",
+  define: {
+    'define.amd': 'false',
+  },
+});
+
+// run with node node_config.mjs
