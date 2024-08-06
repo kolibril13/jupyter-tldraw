c4bb4d8f569ed21af5fef45c53af1e0f388d02a2
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-08-06 12:15:25 +0200

    tweak positions

--- js/reactive_color_picker.jsx
+++ js/reactive_color_picker.jsx
@@ -14,14 +14,13 @@ const render = createRender(() => {
   const handleMount = (editorInstance) => {
     window.myTldrawEditor = editorInstance;
     setEditor(editorInstance);
-
-      const shapes = [
-        { id: createShapeId("rectangle1"), type: "geo", x:  22, y: -47, props: { h:  90, w: 149, color: "black",  dash: "draw", fill: "none",  font: "draw", geo: "rectangle", size: "l",  text: "Color" } },
-        { id: createShapeId("rectangle2"), type: "geo", x: 124, y: 127, props: { h:  75, w: 100, color: "violet", dash: "draw", fill: "solid", font: "draw", geo: "rectangle", size: "xl"          } },
-        { id: createShapeId("rectangle3"), type: "geo", x: 211, y:  88, props: { h: 136, w: 114, color: "orange", dash: "draw", fill: "solid", font: "draw", geo: "rectangle", size: "xl"          } },
-        { id: createShapeId("rectangle4"), type: "geo", x: 265, y:  32, props: { h: 136, w: 114, color: "blue",   dash: "draw", fill: "solid", font: "draw", geo: "rectangle", size: "xl"          } },
-        { id: arrowId,      type: "arrow", x: 100, y: 100 }
-      ];
+    const shapes = [
+      { id: createShapeId("rectangle1"), type: "geo", x:  22, y:  23, props: { h:  90, w: 149, color: "black",  dash: "draw", fill: "none",  font: "draw", geo: "rectangle", size: "l",  text: "Color" } },
+      { id: createShapeId("rectangle2"), type: "geo", x: 124, y: 197, props: { h:  75, w: 100, color: "violet", dash: "draw", fill: "solid", font: "draw", geo: "rectangle", size: "xl"          } },
+      { id: createShapeId("rectangle3"), type: "geo", x: 211, y: 158, props: { h: 136, w: 114, color: "orange", dash: "draw", fill: "solid", font: "draw", geo: "rectangle", size: "xl"          } },
+      { id: createShapeId("rectangle4"), type: "geo", x: 265, y: 102, props: { h: 136, w: 114, color: "blue",   dash: "draw", fill: "solid", font: "draw", geo: "rectangle", size: "xl"          } },
+      { id: arrowId, type: "arrow", x: 100, y: 170 }
+    ];
   
       const bindings = [
         { fromId: arrowId, toId: shapes[0].id, type: "arrow", props: { terminal: "start", normalizedAnchor: { x: 0.5, y: 0.5 } } },
