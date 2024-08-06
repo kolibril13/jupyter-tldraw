73f528d54b61b4bb38c72c2f7a6b8d980ef5b9c1
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-08-06 10:01:22 +0200

    add reactive color picker

--- js/reactive_color_picker.jsx
+++ js/reactive_color_picker.jsx
@@ -1,58 +1,64 @@
 import * as React from "react";
 import { useState } from "react";
 import { createRender, useModelState } from "@anywidget/react";
-import { Tldraw, createShapeId } from "@tldraw/tldraw";
+import { Tldraw, createShapeId, useValue } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
 import "./widget.css";
 
 const render = createRender(() => {
-    const [arrowProperties, setArrowProperties] = useModelState("color");
-  
-    const handleMount = (editor) => {
-      window.myTldrawEditor = editor;
-  
-      const rectangleId1 = createShapeId("rectangle1");
-      const rectangleId2 = createShapeId("rectangle2");
-      const rectangleId3 = createShapeId("rectangle3");
-      const arrowId = createShapeId("arrow");
-  
-      editor.createShapes([
-        { id: rectangleId1, type: "geo", x: 50, y: 50, props: { geo: "rectangle", w: 75, h: 37.5, dash: "draw", color: "blue", size: "m" } },
-        { id: rectangleId2, type: "geo", x: 150, y: 150, props: { geo: "rectangle", w: 50, h: 50, dash: "draw", color: "yellow", size: "m" } },
-        { id: rectangleId3, type: "geo", x: 210, y: 150, props: { geo: "rectangle", w: 100, h: 75, dash: "draw", color: "red", size: "m" } },
-        { id: arrowId, type: "arrow", x: 100, y: 100 }
-      ]);
-  
-      editor.createBindings([
-        { fromId: arrowId, toId: rectangleId1, type: "arrow", props: { terminal: "start", normalizedAnchor: { x: 0.5, y: 0.5 }, isExact: false, isPrecise: false } },
-        { fromId: arrowId, toId: rectangleId2, type: "arrow", props: { terminal: "end", normalizedAnchor: { x: 0.5, y: 0.5 }, isExact: false, isPrecise: false } }
-      ]);
-  
-      editor.store.listen(() => {
-        const arrowBinding = editor.getBindingsInvolvingShape(arrowId);
-        const endToIds = arrowBinding
-        .filter(item => item.props.terminal === "end")
-        .map(item => {
-            const shape = editor.getShape(item.toId);
-            return shape ? shape.props.color : null;
-        }).filter(color => color !== null);
-        
-        if (endToIds) {
-          setArrowProperties(endToIds);
-        }
-      });
-    };
-  
-    return (
-      <>
-        <div style={{ position: "relative", width: 500, height: 300 }}>
-          <Tldraw autoFocus={false} onMount={handleMount} />
-        </div>
-        <div style={{ position: "relative", top: "0px", fontSize: "20px" }}>
-          <pre>{JSON.stringify(arrowProperties, null, 2)}</pre>
-        </div>
-      </>
-    );
-  });
-  
+  const [arrowProperties, setArrowProperties] = useModelState("color");
+  const [editor, setEditor] = useState(null);
+  const arrowId = createShapeId("arrow");
+
+  const handleMount = (editorInstance) => {
+    window.myTldrawEditor = editorInstance;
+    setEditor(editorInstance);
+
+    const shapes = [
+      { id: createShapeId("rectangle1"), type: "geo", x: 50, y: 50, props: { geo: "rectangle", w: 75, h: 37.5, dash: "draw", color: "blue", size: "m" } },
+      { id: createShapeId("rectangle2"), type: "geo", x: 150, y: 150, props: { geo: "rectangle", w: 50, h: 50, dash: "draw", color: "yellow", size: "m" } },
+      { id: createShapeId("rectangle3"), type: "geo", x: 210, y: 150, props: { geo: "rectangle", w: 100, h: 75, dash: "draw", color: "red", size: "m" } },
+      { id: arrowId, type: "arrow", x: 100, y: 100 },
+    ];
+
+    const bindings = [
+      { fromId: arrowId, toId: shapes[0].id, type: "arrow", props: { terminal: "start", normalizedAnchor: { x: 0.5, y: 0.5 }, isExact: false, isPrecise: false } },
+      { fromId: arrowId, toId: shapes[1].id, type: "arrow", props: { terminal: "end", normalizedAnchor: { x: 0.5, y: 0.5 }, isExact: false, isPrecise: false } },
+    ];
+
+    editorInstance.createShapes(shapes);
+    editorInstance.createBindings(bindings);
+  };
+
+  useValue(
+    "binding",
+    () => {
+      const arrowBinding = editor?.getBindingsInvolvingShape(arrowId);
+      const endToIds = arrowBinding
+        ?.filter((item) => item.props.terminal === "end")
+        .map((item) => {
+          const shape = editor.getShape(item.toId);
+          return shape ? shape.props.color : null;
+        })
+        .filter((color) => color !== null);
+
+      if (endToIds) {
+        setArrowProperties(endToIds);
+      }
+    },
+    [editor, arrowId]
+  );
+
+  return (
+    <>
+      <div style={{ position: "relative", width: 500, height: 300 }}>
+        <Tldraw autoFocus={false} onMount={handleMount} />
+      </div>
+      {/* <div style={{ position: "relative", top: "0px", fontSize: "20px" }}>
+        <pre>{JSON.stringify(arrowProperties, null, 2)}</pre>
+      </div> */}
+    </>
+  );
+});
+
 export default { render };
---
+++ marimo_reactive_app.py
@@ -0,0 +1,60 @@
+import marimo
+
+__generated_with = "0.7.17"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def __():
+    import matplotlib.pyplot as plt
+    import numpy as np
+    plt.style.use('_mpl-gallery')
+
+    # make the data
+    np.random.seed(3)
+    x = 4 + np.random.normal(0, 2, 24)
+    y = 4 + np.random.normal(0, 2, len(x))
+    # size and color:
+    sizes = np.random.uniform(15, 80, len(x))
+    opacity = np.random.uniform(0, 1, len(x))
+    return np, opacity, plt, sizes, x, y
+
+
+@app.cell
+def __():
+    from tldraw import ReactiveColorPicker
+    import marimo as mo
+    widget = mo.ui.anywidget(ReactiveColorPicker())
+    widget
+    return ReactiveColorPicker, mo, widget
+
+
+@app.cell
+def __(widget):
+    c = widget.color
+    c
+    return c,
+
+
+@app.cell
+def __(c, np, opacity, plt, sizes, x, y):
+    color = c[0]
+    fig, ax = plt.subplots()
+
+    ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+           ylim=(0, 8), yticks=np.arange(1, 8))
+
+
+    ax.scatter(x, y, s=sizes, c=color, alpha=opacity)
+
+    plt.show()
+    return ax, color, fig
+
+
+@app.cell
+def __():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
