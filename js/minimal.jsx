0394134e40bb2d460c054a2ab5879b37e66c78e2
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2026-05-20 11:59:18 +0200

    add key

--- js/minimal.jsx
+++ js/minimal.jsx
@@ -15,7 +15,7 @@ const render = createRender(() => {
         height: height,
       }}
     >
-      <Tldraw autoFocus={false} />
+      <Tldraw autoFocus={false} licenseKey="tldraw-2026-07-31/WyJQUVo1VG1jbCIsWyIqIl0sMTYsIjIwMjYtMDctMzEiXQ.CkPyjP0F73725rI7q6mqJHPO1raBBtGrGMD4brtu2PaIXIywy8PRtij6fcPZHLws627nS5OuHc2OPquvffbhog" />
     </div>
   );
 });
